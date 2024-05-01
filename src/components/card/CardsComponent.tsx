45797b76ab7337430f5c70f056fca260a45d7f04
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-01 19:32:41 +0330

    responsive design added

--- src/components/card/CardsComponent.tsx
+++ src/components/card/CardsComponent.tsx
@@ -2,11 +2,11 @@ import cardsData from '../../constants/mock-data'
 
 function CardsComponent() {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4 justify-center place-items-center">
       {cardsData.map((card) => (
-        <div key={card.id} className="p-4 rounded">
+        <div key={card.id} className="p-4 rounded text-center">
           <img src={card.image} alt={card.title} className="w-48 mb-2 bg-center" />
-          <p className="text-lg font-semibold text-center">{card.title}</p>
+          <p className="text-lg font-semibold">{card.title}</p>
         </div>
       ))}
     </div>
